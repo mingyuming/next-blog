32813fef9e6efd54e81f4607a877812d001370f7
caimingyu <caimingyu@ishumei.com>
2024-04-10 17:25:26 +0800

    底部适配移动端

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,8 +3,7 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.wrapper}>
-      <span>豫ICP备2020025697号</span>｜<span>Keep on going never give up</span>
-      ｜<span>Copyright © 2023</span>
+      <span>Keep on going never give up</span>｜<span>Copyright © 2024</span>
     </footer>
   );
 };
