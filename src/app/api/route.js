7db72387e86e5cbb87a7a3fd2c82d616e1be436d
caimingyu <caimingyu@ishumei.com>
2024-07-09 10:13:35 +0800

    fix

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,12 +1,11 @@
-import { NextResponse } from 'next/server'
- 
+import {NextResponse} from 'next/server';
+
 export async function GET() {
-  const res = await fetch('http://39.101.169.247/api/home?page=1', {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-  const data = await res.json()
-  console.log(data, 1);
-  return NextResponse.json({ data })
+    const res = await fetch('http://39.101.169.247/api/home?page=1', {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    const data = await res.json();
+    return NextResponse.json({data});
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,5 @@ import styles from './page.module.css';
 
 export default async function Home(props) {
     const {data, info} = await fetch('http://39.101.169.247/api/home?page=1').then(res => res.json());
-
-    return (
-        <HomeContent data={data} info={info} />
-    );
+    return <HomeContent data={data} info={info} />;
 }
